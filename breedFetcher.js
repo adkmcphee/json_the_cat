8b845af72c33eb8bd85b063ea36e0fe84e8b2894
Adam McPhee <adkmcphee@gmail.com>
2023-04-13 23:48:46 +0000

    added tests and index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
-const args = process.argv.slice(2);
-const breed = args[0];
+
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length !== 0) {
+      callback(null, data[0].description);
+      console.log(data[0].description)
+      return;
+    }
+    callback(null, "Breed not found");
+  });
 
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  if (error) {
-    console.log(`Sorry there was an error. See details ${error}`);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length !== 0) {
-    console.log(data);
-    return;
-  }
-  console.log("Breed not found");
-});
-
+module.exports = { fetchBreedDescription };
